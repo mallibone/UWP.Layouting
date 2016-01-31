9e8569c4826331c5a0d8e5de720a0eeb00fbd2c4
Mark Allibone <mark.allibone@noser.com>
2016-01-31 16:36:37 +0100

    Initial commit

---
+++ UWP.Layouting/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace UWP.Layouting.ViewModel
+{
+    internal class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            //if (ViewModelBase.IsInDesignModeStatic)
+            //{
+            //}
+            //else
+            //{
+            //}
+
+            SimpleIoc.Default.Register<ResponsiveLayoutViewModel>();
+            //SimpleIoc.Default.Register<PersonDetailViewModel>();
+        }
+
+        public ResponsiveLayoutViewModel ResponsiveLayout => SimpleIoc.Default.GetInstance<ResponsiveLayoutViewModel>();
+    }
+}
